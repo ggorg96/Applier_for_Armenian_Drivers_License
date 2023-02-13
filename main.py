8be92ad30f10ab80f2cc346eb7f6482058226575
Gor Grigoryan <ggorg96@gmail.com>
2023-02-13 17:35:37 +0400

    added data entering ability for submitting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt6.QtGui import QIntValidator
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
@@ -7,7 +8,7 @@ import threading
 
 url = 'https://roadpolice.am/'
 
-def submit():
+def submit(city_index, public_service_number, phone_number):
     driver = webdriver.Chrome(executable_path='chromedriver/chromedriver_mac_arm64/chromedriver')
     try:
         driver.get(url)
@@ -16,11 +17,16 @@ def submit():
         time.sleep(3)
         driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[2]/span/span[1]/span/span[2]').click()
         time.sleep(3)
-        driver.find_element(By.XPATH, '/html/body/span/span/span[2]/ul/li/ul/li[2]').click()
+        if city_index == 0:
+            driver.find_element(By.XPATH, '/html/body/span/span/span[2]/ul/li/ul/li[2]').click()
+        elif city_index == 1:
+            driver.find_element(By.XPATH, '/html/body/span/span/span[2]/ul/li/ul/li[3]').click()
+        elif city_index == 2:
+            driver.find_element(By.XPATH, '/html/body/span/span/span[2]/ul/li/ul/li[4]').click()
         time.sleep(3)
-        driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[4]/input').send_keys('1234567890')
+        driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[4]/input').send_keys(public_service_number)
         time.sleep(3)
-        driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[5]/input').send_keys('12345678')
+        driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[5]/input').send_keys(phone_number)
         time.sleep(3)
         driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div/div/div[2]/div/form/div[6]/button').click()
         time.sleep(3)
@@ -39,24 +45,43 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle('Road Police')
-        self.setFixedSize(220, 90)
+        self.setFixedSize(220, 210)
+
+        self.city_combo_box = QComboBox(self)
+        self.city_combo_box.addItems(['Yerevan', 'Gyumri', 'Vanadzor'])
+        self.city_combo_box.setFixedSize(210, 30)
+        self.city_combo_box.move(5, 10)
+
+        self.public_service_number = QLineEdit(self)
+        self.public_service_number.setPlaceholderText('Public Service Number')
+        self.public_service_number.setFixedSize(200, 30)
+        self.public_service_number.move(10, 50)
+        self.public_service_number.setValidator(QIntValidator())
+        self.public_service_number.setMaxLength(10)
+
+        self.phone_number = QLineEdit(self)
+        self.phone_number.setPlaceholderText('Phone Number')
+        self.phone_number.setFixedSize(200, 30)
+        self.phone_number.move(10, 90)
+        self.phone_number.setValidator(QIntValidator())
+        self.phone_number.setMaxLength(8)
 
         self.time_out_combo_box = QComboBox(self)
         self.time_out_combo_box.addItems(['Resubmit in each 5 minutes', 'Resubmit in each 10 minutes', 'Resubmit in each 15 minutes'])
         self.time_out_combo_box.setFixedSize(210, 30)
-        self.time_out_combo_box.move(5, 10)
+        self.time_out_combo_box.move(5, 130)
         self.time_out_combo_box.currentIndexChanged.connect(self.thread_time_out_change)
 
         self.start_button = QPushButton(self)
         self.start_button.setText('Start')
         self.start_button.setFixedSize(95, 30)
-        self.start_button.move(10, 50)
+        self.start_button.move(10, 170)
         self.start_button.clicked.connect(self.start_button_handler)
 
         self.stop_button = QPushButton(self)
         self.stop_button.setText('Stop')
         self.stop_button.setFixedSize(95, 30)
-        self.stop_button.move(115, 50)
+        self.stop_button.move(115, 170)
         self.stop_button.clicked.connect(self.stop_button_handler)
         self.stop_button.setDisabled(True)
 
@@ -77,7 +102,7 @@ class MainWindow(QMainWindow):
 
     def submitting_thread(self):
         while not self.stop_thread:
-            submit()
+            submit(self.city_combo_box.currentIndex(), self.public_service_number.text(), self.phone_number.text())
             for i in range(0, self.thread_time_out*60, 3):
                 time.sleep(3)
                 if self.stop_thread:
